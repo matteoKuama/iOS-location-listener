4ee8eb1b70c95547e42985342eed8e16f4ec13b3
francesco <francesco@kuuama.net>
2021-04-29 10:34:53 +0200

    Initial Commit

---
+++ LocationListener/LocationListenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationListenerApp.swift
+//  LocationListener
+//
+//  Created by Kuama on 29/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LocationListenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
