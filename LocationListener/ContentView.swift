e5908f3e946ac5cb03eb62bebe8265af66b58a87
matteoKuama <matteo@kuama.net>
2021-05-03 11:35:32 +0200

    Updating the permission's file Info.plist and adding a demonstration use in ContentView.swift

--- LocationListener/ContentView.swift
+++ LocationListener/ContentView.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    let locationReader = LocationReader()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Get Location", action:{
+            locationReader.setupObserver()
+        }
+        )
+        .padding(.all)
+        
     }
+    
+
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +28,22 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+class LocationReader {
+    
+    private let notificationCenter = NotificationCenter.default
+     
+    func setupObserver(){
+        notificationCenter.addObserver(self, selector: #selector(locationPrinter(_notification:)), name: Notification.Name("Location"), object: LocationRequest.shared)
+    }
+    
+    @objc
+    private func locationPrinter(_notification:Notification){
+        if let data = _notification.userInfo as? [String:Double]{
+            for (name, location) in data {
+                print("\(name): \(location)")
+            }
+        }
+    }
+}
+
